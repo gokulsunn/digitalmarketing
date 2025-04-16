844b715d8e90af2e97e47e8685319da0bb1cf564
suchi <suchiths6528@gmail.com>
2025-04-16 16:33:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Home'
+
+function App() {
+
+  return (
+    <div>
+      <h1>HI Gokul</h1>
+      <Home />
+    </div>
+  )
+}
+
+export default App
